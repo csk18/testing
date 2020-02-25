6f74aff5aca5c98856163a14775e465b278430a9
ckoranga <chandan.koranga1718@gmail.com>
2020-02-25 22:47:07 +0530

    Add dashboard files

---
+++ app/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DashboardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+
+  it(`should have as title 'Dashboard'`, async(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.debugElement.componentInstance;
+    expect(component.title).toEqual('Dashboard');
+  }))
+});
